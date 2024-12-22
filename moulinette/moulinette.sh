f580c144520041b76a3a37d55977d5f878184950
tesla33io <artemstavropolov@gmail.com>
2024-12-22 19:17:44 +0100

    Feat!: refactor moulinette

--- moulinette/moulinette.sh
+++ moulinette/moulinette.sh
@@ -1,39 +1,55 @@
 #!/bin/sh
 
-# Moulinette tester for codechallenges
-# moulinette.sh codechallenge1.c expected result 
+USAGE="Usage: moulinette.sh <level> <source file> <main source file> \
+<expected result (file)>"
 
-export CHALLENGE1=$1
+if [ "$#" -ne 4 ]; then
+    echo "$USAGE"
+    exit 1
+fi
 
-RESULT=/result/1
-# Check for forbidden functions
+LEVEL=$1
+SOURCEF=$2
+MAINF=$3
+EXPECTEDF=$4
+
+if [ ! -f $SOURCEF ]; then
+    echo "Error: source file not found!"
+    exit 1
+fi
+
+if [ ! -f $MAINF ]; then
+    echo "Error: main source file not found!"
+    exit 1
+fi
+
+if [ ! -f $EXPECTEDF ]; then
+    echo "Error: expected result file not found!"
+    exit 1
+fi
+
 if [ ! -f poison ]; then
     echo "Error: poison file not found!"
     exit 1
 fi
 
-# Dont check comments ( `//   `/* ... */`)
-CLEANED_CODE=$(sed -E 's|//.*||; s|/\*[^*]*\*+([^/*][^*]*\*+)*/||g' $CHALLENGE1)
+echo "[WIP]" > /result/$LEVEL
 
-if echo "$CLEANED_CODE" | grep -w -f poison > /dev/null; then
-    # printf "\033[0;31mForbidden function detected !!!! \033[0m\n"
-    printf "[ILLEGAL]\n" >"$RESULT"
-    exit 0
+gcc -w $MAINF $SOURCEF -o out.$LEVEL 2>&1 > /dev/null
+
+nm out.$LEVEL | grep -f poison > /dev/null
+
+if [ $? -eq 0 ]; then
+    printf "[ILLEGAL]\n" > /result/$LEVEL
+    exit 1
 fi
 
-gcc -w $CHALLENGE1 main.c -o out
+TEST_OUTPUT=$(./out.$LEVEL | cat -e)
 
-TEST_RETURN=$(./out | cat -e)
-
-echo "Received: $TEST_RETURN"
-
-if [ "$TEST_RETURN" = "$2" ]; then
-    #printf "\033[0;32m\n\tTest passed !!!! \033[0m\n"
-    printf "[OK]\n" >"$RESULT"
+if [ "$TEST_OUTPUT" = "$(cat $EXPECTEDF | cat -e)" ]; then
+    printf "[OK]\n" > /result/$LEVEL
 else
-    #printf "\033[0;31m\n\tTest failed !!!! \033[0m\n"
-    printf "[KO]\n" >"$RESULT"
+    printf "[KO]\n" > /result/$LEVEL
 fi
 
-rm -fr out
-
+rm -f out.$LEVEL compile_log.$LEVEL
